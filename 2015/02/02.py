7864f958dd421dd3ff710a731dcb7e7850f92ea6
allawitten <allawitten@gmail.com>
2023-04-08 13:20:48 +0200

    Complete 2015/02

--- 2015/02/02.py
+++ 2015/02/02.py
@@ -4,6 +4,7 @@ with open('02.input', 'r') as presents:
 
 # Part 1: find the total paper needed
 def required_paper(l: int, w: int, h: int) -> int:
+    '''Return the wrapping paper required to wrap a present of dimensions (l, w, h)'''
     sides_areas = [l*w, w*h, h*l]
     return 2*sum(sides_areas) + min(sides_areas)
 
@@ -11,6 +12,7 @@ print(sum([required_paper(*dimensions) for dimensions in dimensions_list]))
 
 # Part 2: find the total ribbon needed
 def required_ribbon(l: int, w: int, h: int) -> int:
+    '''Return the ribbon length required to wrap a present of dimensions (l, w, h)'''
     perimeters = [2*(l+w), 2*(w+h), 2*(h+l)]
     return min(perimeters) + w*h*l
 
